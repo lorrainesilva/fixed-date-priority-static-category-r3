03d824aeb9cbcdf078a65c63ac41dd332d45523c
João Victor Rodrigues da Silva <victor.joao.1611@gmail.com>
2018-06-30 12:20:45 -0300

    Initial commit

---
+++ app/src/test/java/reminder/fixeddateprioritystaticcategory/br/unb/cic/reminders/ReminderMainActivityTest.java
@@ -0,0 +1,11 @@
+package reminder.fixeddateprioritystaticcategory.br.unb.cic.reminders;
+
+import org.junit.Test;
+
+public class ReminderMainActivityTest {
+
+    @Test
+    public void DefaultTest() {
+
+    }
+}
